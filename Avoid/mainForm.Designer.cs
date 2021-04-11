1509d4d132d483074a24702f5fbfc558bb9e59be
AAVision <75311052+AAVision@users.noreply.github.com>
2021-04-11 16:49:12 +0300

    Add project files.

---
+++ Avoid/mainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Avoid
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.oathLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // oathLabel
+            // 
+            this.oathLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.oathLabel.AutoSize = true;
+            this.oathLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.oathLabel.Location = new System.Drawing.Point(90, 51);
+            this.oathLabel.Name = "oathLabel";
+            this.oathLabel.Size = new System.Drawing.Size(47, 15);
+            this.oathLabel.TabIndex = 0;
+            this.oathLabel.Text = "label1";
+            this.oathLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Avoid.Properties.Resources._12;
+            this.ClientSize = new System.Drawing.Size(473, 129);
+            this.Controls.Add(this.oathLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "mainForm";
+            this.Text = "Crazy";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label oathLabel;
+    }
+}
+
---
+++ Avoid/mainForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using Microsoft.Win32;
+
+namespace Avoid
+{
+
+    public partial class mainForm : Form
+    {
+      
+        public static class Alliknow
+        {
+            public static bool _isFirstForm = true;
+        }
+        public mainForm()
+        {
+            InitializeComponent();
+            
+        }
+        
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Hide();
+            mainForm newForm1 = new mainForm();
+            mainForm newForm2 = new mainForm();
+            newForm1.Show();
+            newForm2.Show();
+            e.Cancel = true;
+
+
+           //add the executable file to the startup menu
+           // using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+           // {
+           //     key.SetValue("My Program", "\"" + Application.ExecutablePath + "\"");
+           // }
+
+
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            oathLabel.Text = "Thinking How to make this world more safer" + Environment.NewLine + "HAVE FUN!";
+
+            if ((Alliknow._isFirstForm == true))
+                Alliknow._isFirstForm = false;
+            else
+            {
+                //get Maximum Height and Width of the screen
+                int maxHeight = Screen.PrimaryScreen.WorkingArea.Height;
+                int maxWidth = Screen.PrimaryScreen.WorkingArea.Width;
+
+                //random
+                Random ra1 = new Random();
+
+                //set a random position for the generated form
+                int _radomPoint1 = ra1.Next(0, maxWidth - this.Width);
+                int _radomPoint2 = ra1.Next(0, maxHeight - this.Height);
+                this.Location = new Point(_radomPoint1   , _radomPoint2);
+               
+
+          
+                // set Icon for the Forms
+                Icon icon = Icon.ExtractAssociatedIcon("12.ico");
+                this.Icon = icon;
+
+            }
+
+            
+        }
+    }
+}
